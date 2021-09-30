8920440a807424209ed1fc7ef554370164d83dc2
HH <opal6100@naver.com>
2021-09-30 22:55:35 +0900

    0930

---
+++ 01_데이터가져오기.sql
@@ -0,0 +1,10 @@
+주석
+*/
+-- 1. 테이블의 모든 행과 열을 읽어오기, 명령문 실행은 ctrl+ent
+-- *은 전체 열, 명령문 끝에는 세미콜론이 있어야 함.
+SELECT * FROM departments;
+
+-- 2. 특정 열만 조회
+SELECT department_id, department_name
+FROM departments;
+
